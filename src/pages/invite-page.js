5134b265928d5a42d5785a8dafa736e513c04391
Jacky Chen <jacky.chen0319@gmail.com>
2021-07-13 15:11:01 -0700

    Created date box

--- src/pages/invite-page.js
+++ src/pages/invite-page.js
@@ -15,16 +15,20 @@ import Image from 'next/image';
 import eventData from '../../MockData/EventData.js'
 const sampleImg = 'https://wallpaperaccess.com/full/632782.jpg';
 
-const event = eventData.SingleEventData['1'];
+// const event = eventData.SingleEventData['1'];
+const event = eventData.ConfirmedEventData['1'];
+console.log('confirmed event data:', event);
 
 const InvitePage = () => {
   const [open, setOpen] = useState(false);
   const [avail, setAvail] = useState([]); // pass this down to modal
-  const [pending, setPending] = useState(false);
+  const [status, setStatus] = useState('');
 
   useEffect(() => {
     if (event.status === 'pending') {
-      setPending(true);
+      setStatus('pending');
+    } else {
+      setStatus('confirmed');
     }
   }, [])
 
@@ -34,10 +38,20 @@ const InvitePage = () => {
   });
 
   const onSubmit = (data) => {
-    if (avail.length === 0) {
+    if (avail.length === 0 && status === 'pending') {
       alert('Please provide your availability!');
+    } else {
+      // POST REQUEST GOES HERE
+      // which endpoint? what format will the object be in?
+        // POST /rsvp: {name: "jacky", email: "jacky@gmail.com"}
+      console.log('submitted data:', data);
     }
   }
+
+  const dateDay = (date) => {
+    return date.substring(10, 8);
+  }
+  dateDay(event.window.start)
 
   const handleOpen = (e) => {
     e.preventDefault();
@@ -54,70 +68,72 @@ const InvitePage = () => {
 
       <div className={styles.container}>
 
-      <Image
-        src={sampleImg}
-        className={styles.photo}
-        layout="responsive"
-        height={144}
-        width={1050}
-        alt="event-image"
-      />
+        <Image
+          src={sampleImg}
+          className={styles.photo}
+          layout="responsive"
+          height={144}
+          width={1050}
+          alt="event-image"
+        />
+
+        <Box className={styles.date_box} boxshadow={3}>
+          <div className={styles.date_banner}></div>
+          <div className={styles.date}>{dateDay(event.window.start)}</div>
+        </Box>
 
         <div className={styles.body}>
           <div className={styles.info}>
-
             <div className={styles.details}>
-
               <Box className={styles.header} boxShadow={3}>
                 <div className={styles.invite_title}>{event.owner} invites you to join {event.name}!</div>
                 <div className={styles.subtitle}>
-                  {pending &&
+                  {status === 'pending' &&
                   <div className={styles.status}>Pending</div>
                   }
                   <div className={styles.location}>{event.location}</div>
                 </div>
               </Box>
-
               <Box className={styles.description} boxShadow={3}>
                 {event.description}
               </Box>
             </div>
         </div>
+
           <Box className={styles.form} boxShadow={3}>
-          <form onSubmit={handleSubmit(onSubmit)}  boxShadow={3}>
-            <label>Name:</label>
-            <input className={styles.input} {...register('name', { required: 'Please enter your name.' })}/>
-            <label>Email:</label>
-            <input type="email" className={styles.input} {...register('email', { required: 'Please enter your email.' })}/>
-            {pending &&
-            <button className={styles.submit_form} onClick={handleOpen}>Add Availability</button>
-            }
-              <Modal
-                aria-labelledby="transition-modal-title"
-                aria-describedby="transition-modal-description"
-                open={open}
-                className={styles.modal}
-                onClose={handleClose}
-                closeAfterTransition
-                BackdropComponent={Backdrop}
-                BackdropProps={{
-                  timeout: 500,
-                }}
-              >
-                <Fade in={open}>
-                  <div>
-                    <h2 id="transition-modal-title">Transition modal</h2>
-                    <p id="transition-modal-description">react-transition-group animates me.</p>
-                  </div>
-                </Fade>
-              </Modal>
-            <input type="submit" className={styles.rsvp_btn} value="RSVP"/>
-          </form>
+            <form onSubmit={handleSubmit(onSubmit)}  boxShadow={3}>
+              <label>Name:</label>
+              <input className={styles.input} {...register('name', { required: 'Please enter your name.' })}/>
+              <label>Email:</label>
+              <input type="email" className={styles.input} {...register('email', { required: 'Please enter your email.' })}/>
+              {status === 'pending' &&
+              <button className={styles.submit_form} onClick={handleOpen}>Add Availability</button>
+              }
+                <Modal
+                  aria-labelledby="transition-modal-title"
+                  aria-describedby="transition-modal-description"
+                  open={open}
+                  className={styles.modal}
+                  onClose={handleClose}
+                  closeAfterTransition
+                  BackdropComponent={Backdrop}
+                  BackdropProps={{
+                    timeout: 500,
+                  }}
+                >
+                  <Fade in={open}>
+                    <div>
+                      <h2 id="transition-modal-title">SAMPLE MODAL</h2>
+                      <p id="transition-modal-description">This is a placeholder for the Add Availability modal window.</p>
+                    </div>
+                  </Fade>
+                </Modal>
+              <input type="submit" className={styles.rsvp_btn} value="RSVP"/>
+            </form>
           </Box>
         </div>
       </div>
     </div>
-
   )
 };
 
