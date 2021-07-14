753fca2d90a189c0558836a6bc88493183621958
Derek <maloned@uci.edu>
2021-07-14 15:17:55 -0700

    Add put requests to the update event form to give user ability to update locaiton, duration and description

--- src/components/UpdateEventForm.js
+++ src/components/UpdateEventForm.js
@@ -3,6 +3,7 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import styles from '../styles/Event.module.css';
 import { useForm, Controller } from 'react-hook-form';
+import axios from 'axios';
 const UpdateEventForm = (props) => {
   const [open, setOpen] = useState(false);
   const {register, handleSubmit, control } = useForm({
@@ -13,8 +14,41 @@ const UpdateEventForm = (props) => {
     }
   })
   const onSubmit = data => {
-    console.log(data);
-    setOpen(false)
+    let updates = [];
+    data.duration = data.duration * 3600
+    for (var k in data) {
+      //console.log(data[k])
+      if (data[k] !== props.data[k]) {
+        updates.push(
+          {
+            "where": {
+                "property": "_id",
+                "value": props.data._id
+            },
+            "what": {
+                "method": "$set",
+                "field": k,
+                "value": data[k]
+            }
+        })
+      }
+    }
+    console.log(updates)
+    var config = {
+      method: 'put',
+      url: 'http://localhost:3000/api/events',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data : {updates: updates}
+    };
+    axios(config).then( res => {
+      setOpen(false);
+      props.refeshData()
+    }).catch(err => {
+      console.log(err)
+    })
+    //setOpen(false)
   }
   if(open) {
     return (
@@ -30,7 +64,7 @@ const UpdateEventForm = (props) => {
             <h3>
               Duration:
             </h3>
-            <input type="number" {...register("duration")}/> hours
+            <input step="0.5" type="number" {...register("duration")}/> hours
           </label>
           <label>
             <h3>Description:</h3>
