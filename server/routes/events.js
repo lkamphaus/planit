cdfbb8be5b03dbdcb1bb0f535e3a31a0fc092160
Jacob Santala <JacobISantala@gmail.com>
2021-07-14 10:32:41 -0700

    Added the route for uploading photos.

--- server/routes/events.js
+++ server/routes/events.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cloudinary = require('cloudinary').v2;
 const { fetchEvents, addEvent, updateEvent } = require('../../database/controllers/eventController');
 const eventRouter = express.Router();
+require('dotenv').config();
 
 eventRouter.get('/', async (req, res) => {
   const { options } = req.body; // object
@@ -34,5 +36,26 @@ eventRouter.post('/', async (req, res) => {
     res.sendStatus(400);
   }
 });
+
+eventRouter.post('/photos', async (req, res) => {
+  const { fileData } = req.body;
+  let confObj = {
+    cloud_name: `${process.env.CLOUD_NAME}`,
+    api_key: `${process.env.API_KEY}`,
+    api_secret: `${process.env.API_SECRET}`,
+  };
+  cloudinary.config(confObj);
+  cloudinary.uploader.upload(fileData, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.sendStatus(400);
+    } else {
+      const transform = 'w_400,c_scale/';
+      const insertInd = result.url.indexOf('upload/') + 7;
+      const transformedUrl = result.url.slice(0, insertInd) + transform + result.url.slice(insertInd);
+      res.status(200).send(transformedUrl);
+    }
+  });
+})
 
 module.exports.eventRouter = eventRouter;
