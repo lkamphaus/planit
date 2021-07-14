aaece21afba03e8af40481f9414c4599e376d482
Ivan Bailey <ivangbailey@gmail.com>
2021-07-14 09:38:50 -0700

    fixed merge conflict and reconfigured events router correctly

--- server/index.js
+++ server/index.js
@@ -5,15 +5,10 @@ const { loginRouter } = require('./routes/login.js');
 const app = express();
 
 
-<<<<<<< HEAD
 const initRoutes = (httpServer) => {
-  httpServer.use('/events', eventRouter);
+  httpServer.use('/api/events', eventRouter);
   httpServer.use('/', loginRouter);
 };
-=======
-app.use('/api/events', eventRouter);
-app.use('/', loginRouter);
->>>>>>> main
 
 // express.get('/logout', (req, res) => {
 //   res.send('<h1>Logout</h1>')
