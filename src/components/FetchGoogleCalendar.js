34059b697f7dc819a7f5b4d430f0991f653a91f9
chefferson <49037676+chefferson@users.noreply.github.com>
2021-07-15 00:04:59 -0700

    Plan.it can now automatically fetch a guest's Google calendar to pre-populate their free blocks in the availability calendar - Added FetchGoogleCalendar component, which renders a button with some explanatory text and contains logic for authenticating with the Google API and eliciting Google credentials from the user - Added googleCalendarUtils with computeAvailability function that takes a guest's busy periods and inverts them, returning their free periods - Added FetchGoogleCalendar.module.css to style the FetchGoogleCalendar component, mainly layout - TimeBlock componentn now renders the FetchGoogleCalendar button above the interactive calendar - The Google Client ID is stored in env.local, fetched by the invite-page using getStaticProps, and passed down through the Availability component and the TimeBlock component to the FetchGoogleCalendar component - Added the env.local.example file with empty config variables, since env.local is gitignored

---
+++ src/components/FetchGoogleCalendar.js
@@ -0,0 +1,123 @@
+import styles from '../styles/FetchGoogleCalendar.module.css';
+import React, { useState, useEffect } from 'react';
+import { Button } from '@material-ui/core';
+import computeAvailability from './helpers/googleCalendarUtils';
+
+const FetchGoogleCalendar = ({
+  windowStart = '2021-07-12T15:00:00-07:00',
+  windowEnd = '2021-07-18T15:00:00-07:00',
+  storeGoogleAvailability,
+  googleClientId
+}) => {
+
+  // Variables for Google API authentication
+  var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+  var SCOPES = "https://www.googleapis.com/auth/calendar.events.freebusy";
+
+  const [status, setStatus] = useState('');
+  const [calendarDataIsLoading, setCalendarDataIsLoading] = useState(false);
+  const [showNoCalendarDataMsg, setShowNoCalendarDataMsg] = useState(false);
+
+  // Initialize Google API auth once component has mounted
+  useEffect(() => {
+    gapi.load('client:auth2', initClient);
+  }, []);
+
+  // Clear the 'no calendar data' message after three seconds
+  useEffect(() => {
+    if (showNoCalendarDataMsg) {
+      setTimeout(() => {
+        setShowNoCalendarDataMsg(false);
+      }, 3000);
+    }
+  }, [showNoCalendarDataMsg])
+
+  /**
+   *  Initialize the Google API client library
+   */
+  const initClient = () => {
+    return gapi.client.init({
+      clientId: googleClientId,
+      discoveryDocs: DISCOVERY_DOCS,
+      scope: SCOPES
+    })
+      .then(() => {}, (error) => {
+        console.log(error);
+      });
+  }
+
+  /**
+   *  Sign in the user to Google upon button click, fetch events, log user out of Google
+   */
+  const handleClick = (event) => {
+    return gapi.auth2.getAuthInstance().signIn()
+      .catch((err) => {
+        console.log('Error attempting to sign into Google');
+        console.log(err);
+      })
+      .then(() => {
+        console.log('User logged into Google successfully');
+        return loadEvents();
+      })
+      .catch((err) => {
+        console.log('Error attempting to fetch event data from Google');
+        console.log(err);
+      })
+      .then(() => {
+        setCalendarDataIsLoading(true);
+        return gapi.auth2.getAuthInstance().signOut();
+      })
+      .catch((err) => {
+        setCalendarDataIsLoading(false);
+        console.log('Error logging user out of Google');
+      })
+      .then(() => {
+        setCalendarDataIsLoading(false);
+        console.log('User logged out of Google successfully');
+      });
+  };
+
+  /**
+   * Fetch the events from the user's Google Calendar and pass them to the storeGoogleAvailability function from props
+   */
+  const loadEvents = () => {
+    return gapi.client.calendar.events.list({
+      'calendarId': 'primary',
+      'timeMin': (new Date(windowStart)).toISOString(),
+      'timeMax': (new Date(windowEnd)).toISOString(),
+      'showDeleted': false,
+      'singleEvents': true,
+      'orderBy': 'startTime'
+    })
+      .then((response) => {
+        const events = response.result.items;
+        if (events.length > 0) {
+          const window = {
+            start: windowStart,
+            end: windowEnd,
+          };
+          const availability = computeAvailability(window, events);
+          storeGoogleAvailability(availability);
+        } else {
+          setShowNoCalendarDataMsg(true);
+        }
+      });
+  };
+
+  return (
+    <div className={styles.container}>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleClick}
+        disabled={calendarDataIsLoading}
+      >
+        {calendarDataIsLoading ? 'Loading Calendar Data' : 'Fetch Google Calendar'}
+      </Button>
+      {showNoCalendarDataMsg && <p className={styles.noCalendarDataMsg}>No events found</p>}
+      <p className={styles.googleFetchHelpText}><b>Plan.it</b> can fill in your availability automatically from your Google Calendar. You will be able to adjust your availability before submitting.</p>
+    </div>
+  );
+};
+
+export default FetchGoogleCalendar;
